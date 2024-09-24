643c377cef8bd5c157ef8a00dc17240a732253f9
Supertrex4444 <caytont4247@student.faytechcc.edu>
2024-09-24 14:21:21 -0400

    First Upload

---
+++ P1LAB1_CaytonTravis.py
@@ -0,0 +1,21 @@
+# Travis Cayton
+# 9/24/2024
+# P1Lab1
+# Get input from the user and display output.
+
+
+
+# Prompt the user to enter their first name
+firstName = input("Enter your first name: ")
+
+# Prompt the user to enter their last name
+lastName = input("Enter your last name: ")
+
+# Greet the user using the information they provided
+print("Hello,", firstName, lastName  + "!", "Welcome to CTI-110")
+
+
+
+
+#If the program is ran directly from the file without using IDLE, this will keep it from closing itself
+while True: ""
